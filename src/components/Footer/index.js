d58f962df5c6009775c391445e92f9dcfb43b303
robson-sales <eng.robs@gmail.com>
2023-03-09 15:21:13 -0300

    Otimização de imagens e ajuste no footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <div className="footer">
       <p>
-        Desenvolvido com ❤️ por beeks <br />
-        <br />© 2023 Todos os direitos reservados
+        Desenvolvido com ❤️ por <a href="https://www.beeks.com.br">beeks</a> | ©
+        2023 Todos os direitos reservados
       </p>
     </div>
   );
