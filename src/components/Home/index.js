5fdaeba5f3f7a2fe09549bd46e81abd0bd850f2b
robson-sales <eng.robs@gmail.com>
2023-03-16 11:07:21 -0300

    Otimização de imagens e código

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 import './style.css';
-import banner from '../../assets/images/home-banner.jpg';
 
 export default function Home() {
   const navigate = useNavigate();
@@ -11,12 +10,6 @@ export default function Home() {
 
   return (
     <div className="banner">
-      <div className="banner-img">
-        <img
-          src={banner}
-          alt="Uma mulher trabalhando segurando um smartphone"
-        />
-      </div>
       <div className="text-over-left">
         <h2>
           Coloque a tecnologia <br />
